5aaa51b752742a1d471fe0f98b44fbbd5c4dbb02
Chris Fredricksen <cmfredricksen@gmail.com>
2022-10-18 19:43:13 -0500

    added api endpoint

---
+++ src/routes/api/recipes/server.js
@@ -0,0 +1,8 @@
+import { fetchRecipes } from '$lib/utils/fetchRecipes';
+import { json } from '@sveltejs/kit';
+
+export const GET = async () => {
+	const recipes = await fetchRecipes();
+
+	return json(recipes);
+};
